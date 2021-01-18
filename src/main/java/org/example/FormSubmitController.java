d56458c916ec779854b4af7f687f48a05e060f6e
TranNhutKhaKHTN <trannhutkha123456@gmail.com>
2021-01-19 00:24:38 +0700

    integer validator

--- src/main/java/org/example/FormSubmitController.java
+++ src/main/java/org/example/FormSubmitController.java
@@ -24,31 +24,22 @@ public class FormSubmitController {
     public Button primaryButton;
 
     @FXML
-<<<<<<< HEAD
-    @FXRequired(message = "required field!")
-    @FXInteger(min = 3, max = 30,message = "khong phai interger!")
-    TextField tf1;
-
-    @FXML
-    @FXNumber(min = 10,max = 100)
-=======
-
+//    @FXInteger(message = "Input value is not integer")
     TextField tf1;
 
     @FXML
 //    @FXRequired(message = "required field!")
     //@FXNumber(min = 10,max = 100)
     @FXValidation(validation = CustomValidator.class, message = "k dc")
->>>>>>> 0ccd3d4b7f9eaf863e14eab9de67ed202897856d
     TextField tf2;
 
     @FXML
+    @FXRequired(message = "field is required!")
     @FXString(max = 10, min = 5, message = "độ dài filed không hợp lệ")
     TextField tf3;
 
     @FXML
     @FXRegex(regex = "^[a-zA-Z][\\w-]+@([\\w]+\\.[\\w]+|[\\w]+\\.[\\w]{2,}\\.[\\w]{2,})$")
-    @FXRequired(message = "required field!")
     TextField tf4;
 
     @FXML
@@ -57,35 +48,24 @@ public class FormSubmitController {
     @FXML
     private void submit() {
 //        App.setRoot("secondary");
-<<<<<<< HEAD
-        FXValidationHandler handler = new FXValidationHandlerWithLabel(FormSubmitController.class, anchorPane);
-//        handler.addValidate("REQUIRED", tf1, Arrays.asList("Loi ne"));
-//        handler.addValidate("REQUIRED", tf2, Arrays.asList("Lai loi ne"));
-=======
         FXValidationHandler handler = FactoryHandlerProducer.getHandler("LABEL");
         handler.initialize(FormSubmitController.class, anchorPane);
         handler.addValidate("REQUIRED", tf1, Arrays.asList("Invalid"));
->>>>>>> 0ccd3d4b7f9eaf863e14eab9de67ed202897856d
         handler.executeValidate();
 //        Validator.valid("STRING", "dkfd", 2, 3, tf1);
 //        new DialogErrorHandler().display(new ArrayList<String>(Arrays.asList("Độ dài không hợp lệ", "Email không phù hợp")));
 
       /*  Class<?> aClazz = FormSubmitController.class;
-
         //lấy cái Field khai báo là tf3 ra
         Field field3 = aClazz.getDeclaredField("tf3");
         //lấy annotation FXString của Filed
         Annotation an = (Annotation) field3.getDeclaredAnnotation(FXString.class);
-
         // tạo cái String validator để validate
         StringValidator rs = new StringValidator();
-
         // gọi hàm validate tf3 với cái annotation là FXString an
         rs.validate(tf3, (FXString) an);
-
         // tương tự vs các field khác
         //...
-
         // Chúng ta cũng có thể sử dụng
         // StringValidator stringValidator = new StringValidator(tf3, (FXString) an);
         // stringValidator.validate();*/
